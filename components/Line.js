8ba4554f0a2f91aadf4a7da29f84376a0a2c5880
Adrien Gaudon <adrien.gaudon@liveblocks.io>
2022-07-14 16:40:44 -0400

    remove .next folder

--- components/Line.js
+++ components/Line.js
@@ -3,8 +3,9 @@ import React, { useState } from 'react';
 import styles from "./Line.module.css";
 
 export default function Line(props) {
-
-    const handleRevealerButtonClick = (event) => { null }
+    const [isTextVisible, setIsTextVisible] = useState(true);
+    
+    const handleRevealerButtonClick = (event) => { setIsTextVisible(event.target.value) }
 
     return (
         <li className={props.isHighlighted ? styles.highlightedLine : styles.line }>
@@ -16,14 +17,14 @@ export default function Line(props) {
                     👁&nbsp;
                 <input
                     type="checkbox" id={"revealer-"+props.lineId} name={"revealer-"+props.lineId} value={"revealer-"+props.lineId}
-                    
                     onClick={(event) => handleRevealerButtonClick(event)} />
                     )&nbsp;
                 </label>
             </span> 
 
-            <span className={!props.hideHighlightedLines || (props.hideHighlightedLines && !props.isHighlighted) ?  styles.textShow : styles.textHide} >
-                {props.text}
+            {/* <span className={!props.hideHighlightedLines || (props.hideHighlightedLines && !props.isHighlighted) ?  styles.textShow : styles.textHide} > */}
+            <span className={isTextVisible ?  styles.textShow : styles.textHide} >
+                {isTextVisible} -  {props.text}
             </span>
         </li>
     );
