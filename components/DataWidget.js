029e9a90c4810a6c9015edf14806a0b06c588eaf
Adri <adrien.gaudon@liveblocks.io>
2022-08-31 14:35:06 -0400

    end of data widget

--- components/DataWidget.js
+++ components/DataWidget.js
@@ -5,6 +5,7 @@ import styles from "./DataWidget.module.css";
 export default function DataWidget(props) {
 
     let flattenedListOfLines = [];
+    let flattenedListOfCharacters = [];
 
     props.script.sections.forEach(section =>
         section.lines.forEach(line => flattenedListOfLines.push({
@@ -13,44 +14,57 @@ export default function DataWidget(props) {
             characterId: line.characterId,
             text: line.text
         })));
-    
-
-    //TODO HOW CAN I TRANSFORM A LIST INTO NEW OBJECTS BUT KEEP IT AS LIST??
-    let charactersSummary = [];
 
     props.cast.forEach(character => {
-        charactersSummary.push(
-            {
-                id: character.id,
-                displayName: character.displayName,
-                numberOfLines: getNumberOfLines(character.id),
-                numberOfWords: getNumberOfWords(character.id)
-            }
-        )
+        let numberOfLines = getNumberOfLinesPerCharacter(character.id);
+        if(numberOfLines>0)
+        {
+            flattenedListOfCharacters.push(
+                {
+                    id: character.id,
+                    displayName: character.displayName,
+                    numberOfLines: getNumberOfLinesPerCharacter(character.id),
+                    numberOfWords: getNumberOfWordsPerCharacter(character.id)
+                }
+            )
+        }
     });
-
-    function getNumberOfLines(characterId) {
-        let lines = flattenedListOfLines.filter(l=> l.characterId == characterId);
-        let number = lines.length;
-        return number;
+    
+    let dataSummary = 
+    {
+        numberOfSections: props.script.sections.length, 
+        numberOfCharacters: flattenedListOfCharacters.length, 
+        //TODO WHAT WOULD BE THE BEXT WAY TO GET THAT NUMBER?
+        numberOfLines:flattenedListOfCharacters.map(c=> c.numberOfLines).reduce((prev, curr) => prev + curr, 0), 
+        numberOfWords:flattenedListOfCharacters.map(c=> c.numberOfWords).reduce((prev, curr) => prev + curr, 0), 
+        characters:flattenedListOfCharacters
     };
-    function getNumberOfWords(characterId) {
-        let numberOfWords = 0;
-        let lines = flattenedListOfLines.filter(l=> l.characterId == characterId);
-        lines.forEach(l=> numberOfWords= numberOfWords + l.text.split(' ').length);
-        return numberOfWords;
+    
+
+    function getNumberOfLinesPerCharacter(characterId) {
+        return flattenedListOfLines.filter(l=> l.characterId == characterId).length;
+    };
+    function getNumberOfWordsPerCharacter(characterId) {
+        return flattenedListOfLines.filter(l=> l.characterId == characterId).map(l=> l.text.split(' ').length).reduce((prev, curr) => prev + curr, 0);
     };
 
     const renderDataCharacter = (character) => {
         return (
-            <li>{character.displayName}, {character.numberOfLines} lines, {character.numberOfWords} words</li>
+            <li>{character.numberOfLines} for {character.displayName} ({character.numberOfWords} words)</li>
         );
     };
 
     return (
         <div>
             <ul>
-                {charactersSummary.map(character => renderDataCharacter(character))}
+                <li>{dataSummary.numberOfSections} sections</li>
+                <li>{dataSummary.numberOfCharacters} characters</li>
+                <li>{dataSummary.numberOfLines} lines and {dataSummary.numberOfWords} words</li>
+                <li>
+                    <ul>
+                        {dataSummary.characters.sort().map(character => renderDataCharacter(character))}
+                    </ul>
+                </li>
             </ul>
         </div>
     );
